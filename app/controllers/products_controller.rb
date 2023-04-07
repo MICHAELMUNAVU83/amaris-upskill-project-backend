798a6b61372ef6c063d2e1f03fdb7df20f6cb6ec
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2023-04-07 21:26:28 +0300

    Added resources

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,46 @@
 class ProductsController < ApplicationController
+    skip_before_action :authorize
+
+    def index
+        @products = Product.all
+        render json: @products , include: :reviews ,:
+    end
+
+
+    def show
+        @product = Product.find(params[:id])
+        render json: @product
+    end
+
+    def create
+        @product = Product.new(product_params)
+        if @product.save
+            render json: @product
+        else
+            render json: @product.errors, status: :unprocessable_entity
+        end
+    end
+
+    def update
+
+        @product = Product.find(params[:id])
+        if @product.update(product_params)
+            render json: @product
+        else
+            render json: @product.errors, status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        @product = Product.find(params[:id])
+        @product.destroy
+    end
+
+    private
+        def product_params
+            params.require(:product).permit(:name, :description, :price, :image)
+        end
+
+
+
 end
