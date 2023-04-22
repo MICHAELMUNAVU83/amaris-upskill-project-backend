501d0b4d4a7b286e0b7fd68b1a0db32d5d7ad1a0
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2023-04-22 11:31:39 +0300

    Added

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,6 @@
 class ProductsController < ApplicationController
     skip_before_action :authorized
+    rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
 
     def index
         @products = Product.all
@@ -8,8 +9,9 @@ class ProductsController < ApplicationController
 
 
     def show
-        @product = Product.find(params[:id])
+        @product = find_record(params[:id])
         render json: @product
+
     end
 
     def create
@@ -37,6 +39,15 @@ class ProductsController < ApplicationController
     end
 
     private
+    def find_record(id)
+        Product.find(id)
+      rescue ActiveRecord::RecordNotFound
+        raise ApiError.new("Record not found")
+      end
+      
+      def record_not_found
+        render json: { error: "Record not found" }, status: :not_found
+      end
         def product_params
             params.require(:product).permit(:name, :description, :price, :image)
         end
--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -1,5 +1,8 @@
 class ProductSerializer < ActiveModel::Serializer
   attributes :id, :name, :description, :image, :price
+  
+ 
+  
   has_many :reviews
   has_many :like
 end
